73b07a83c64255e2a2a58cf8fc2fabfff09c8521
leixin <realleixin@qq.com>
2025-09-17 18:36:58 +0800

    [feat] load papergen config when launching ap

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,5 @@
 from src.app import create_app
+from src.config import conf
 
 app = create_app()
 
@@ -6,4 +7,4 @@ app = create_app()
     测试
 """
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=50086, debug=False, processes=1, threaded=False)
+    app.run(host=conf["papergen"]["host"], port=conf["papergen"]["http_port"], debug=False, processes=1, threaded=False)
