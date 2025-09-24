e5be210b12a6f400d47ef3ded8019e2d26b205bd
leixin <realleixin@qq.com>
2025-09-24 10:20:56 +0800

    Feat: introduce minio_storage infrastrucutre layer

--- src/infrastructure/storage/minio_storage.py
+++ src/infrastructure/storage/minio_storage.py
@@ -5,79 +5,70 @@ import base64
 from minio import Minio
 from minio.error import S3Error
 
-_minio_client = None
 
-
-def _get_minio_client():
-    global _minio_client
-    if _minio_client is None:
-        try:
-            _minio_client = Minio(
+class MinioStorage:
+    
+    def __init__(self):
+        self.client = Minio(
                 endpoint=f"{os.getenv("MINIO_HOST")}:{os.getenv("MINIO_PORT")}",
                 access_key=os.getenv("MINIO_USER"),
                 secret_key=os.getenv("MINIO_PASSWORD"),
                 secure=False
             )
-        except S3Error:
+        self.bucket = os.getenv("MINIO_BUCKET")
+
+
+    def _create_bucket(self, client, bucket):
+        if not client.bucket_exists(bucket):
+            client.make_bucket(bucket)
+
+
+    def save_code(self, request_id: str, page_id: str, code: str):
+        try: 
+            code_bytes = code.encode("utf-8") # str to bytes stream
+            object_name = f"{request_id}/code/task_{page_id}.tsx"
+            
+            # upload to MinIO
+            self.client.put_object(
+                bucket_name= self.bucket,
+                object_name = object_name,
+                data = io.BytesIO(code_bytes),
+                length = len(code_bytes),
+                content_type = "text/typescript-jsx"
+            )
+            return f"http://localhost:{os.getenv("MINIO_CONSOLE_PORT")}/{self.bucket}/{object_name}"
+            
+        except S3Error as e:
             raise
-    return _minio_client
-
-def _create_bucket(client, bucket):
-    if not client.bucket_exists(bucket):
-        client.make_bucket(bucket)
-
-
-def save_code(request_id: str, page_id: str, code: str):
-    try: 
-        client = _get_minio_client()
-        bucket = os.getenv("MINIO_BUCKET")
-        code_bytes = code.encode("utf-8") # str to bytes stream
-        object_name = f"{request_id}/code/task_{page_id}.tsx"
-        
-        # upload to MinIO
-        client.put_object(
-            bucket_name= bucket,
-            object_name = object_name,
-            data = io.BytesIO(code_bytes),
-            length = len(code_bytes),
-            content_type = "text/typescript-jsx"
-        )
-        return f"http://localhost:{os.getenv("MINIO_CONSOLE_PORT")}/{bucket}/{object_name}"
-        
-    except S3Error as e:
-        raise
     
     
-def save_img(request_id: str, page_id: str, img: str):
-    try:
-        client = _get_minio_client() 
-        bucket = os.getenv("MINIO_BUCKET")
-        object_name = f"{request_id}/img/task_{page_id}.png"
-        
-        # base64 to binary stream
-        if img.startswith("data:image"):
-            img_base64 = img.split(",")[1]
-        else:
-            img_base64 = img
-        img_bytes = base64.b64decode(img_base64, validate=True)
-        
-        # upload to Minio
-        client.put_object(
-            bucket_name = os.getenv("MINIO_BUCKET"),
-            object_name = object_name,
-            data = io.BytesIO(img_bytes),
-            length=len(img_bytes),
-            content_type="image/png"
-        )
-        return f"http://localhost:{os.getenv("MINIO_CONSOLE_PORT")}/{bucket}/{object_name}"
-        
-    except S3Error as e:
-        raise
+    def save_img(self, request_id: str, page_id: str, img: str):
+        try:
+            object_name = f"{request_id}/img/task_{page_id}.png"
+            
+            # base64 to binary stream
+            if img.startswith("data:image"):
+                img_base64 = img.split(",")[1]
+            else:
+                img_base64 = img
+            img_bytes = base64.b64decode(img_base64, validate=True)
+            
+            # upload to Minio
+            self.client.put_object(
+                bucket_name = self.bucket,
+                object_name = object_name,
+                data = io.BytesIO(img_bytes),
+                length=len(img_bytes),
+                content_type="image/png"
+            )
+            return f"http://localhost:{os.getenv("MINIO_CONSOLE_PORT")}/{bucket}/{object_name}"
+            
+        except S3Error as e:
+            raise
 
 
-def check_minio_health():
-    try:
-        client = _get_minio_client()
-        _create_bucket(client, os.getenv("MINIO_BUCKET"))
-    except S3Error as e:
-        raise
+    def check_storage_health(self):
+        try:
+            self._create_bucket(self.client, self.bucket)
+        except S3Error as e:
+            raise
