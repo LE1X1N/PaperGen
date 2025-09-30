94906620c2027be4f76b1f51a1f2a23e7084d1b0
Xin Lei <realleixin@qq.com>
2025-09-30 18:50:58 +0800

    refactor: add decorator for generation functions

--- services/paper_service/app.py
+++ services/paper_service/app.py
@@ -5,12 +5,12 @@ if __name__== "__main__":
     title = "基于Android+XAMPP+MySQL的家校互动平台设计与实现"
     
     generate_image_json(
-        title,
+        title=title,
         save=True,
-        save_path="services/paper_service/test/image_json_test.json"
+        save_path="test/image_json_test.json"
     )
     
     generate_paper_structure( 
-        title,
+        title=title,
         save=True,
-        save_path="services/paper_service/test/structure_test.json")
+        save_path="test/structure_test.json")
--- services/paper_service/src/service/gen_funcs.py
+++ services/paper_service/src/service/gen_funcs.py
@@ -1,54 +1,50 @@
 import json
+from typing import Dict
 from src.llm.prompt import PAPER_STRUCTURE_PROMPT, IMAGE_JSON_PROMPT
 from src.llm.client import call_chat_completion
 
 
-def generate_paper_structure(title: str=None, save: bool=False, save_path: str=None):
-    
-    user_prompt = f"""
-    请分析论文题目《{title}》，设计该论文结构结构。
+def llm_json_generator(system_prompt: str) -> callable:
     """
-    messages=[
-            {'role':'assistant', 'content': PAPER_STRUCTURE_PROMPT},
-            {'role': 'user', 'content': user_prompt}
-    ]
-
-    try:
-        raw_res = call_chat_completion(messages)
-        res = json.loads(raw_res)
-        
-        if not save:
-            return res
-        
-        with open(save_path, 'w', encoding='utf-8') as f:
-            json.dump(res, f, indent=2, ensure_ascii=False)
-    
-    except json.JSONDecodeError:
-        print("不是有效JSON格式！")
-        print(res)
-
-
-def generate_image_json(title: str=None, save: bool=False, save_path: str=None):
-    user_prompt = f"""
-    请分析论文题目《{title}》，设计该家校互动平台的页面结构，包括所有必要的页面模块、具体页面及其功能描述。
-    按照要求的JSON格式输出，确保涵盖不同角色的功能页面（如管理员、用户等角色）。
+        a decorator which encapsulate llm calling, JSON parsing and file saving
     """
+    print("decorator function")
     
-    messages=[
-        {'role':'assistant', 'content': IMAGE_JSON_PROMPT},
-        {'role': 'user', 'content': user_prompt}
-    ]
-    
-    try:
-        raw_res = call_chat_completion(messages)
-        res = json.loads(raw_res)
+    def decorator(func):    # input is a function
         
-        if not save:
-            return res
+        def wrapper(*args, **kwargs) -> Dict:
+            func(*args, **kwargs)   # run initial function
+            
+            print("wrapper function")
+            title = kwargs["title"]
+            save = kwargs["save"]
+            save_path = kwargs["save_path"]
         
-        with open(save_path, 'w', encoding='utf-8') as f:
-            json.dump(res, f, indent=2, ensure_ascii=False)
-    
-    except json.JSONDecodeError:
-        print("不是有效JSON格式！")
-        print(res)
+            messages = [
+                {'role': 'assistant', 'content': system_prompt},
+                {'role': 'user', 'content': f"请分析论文题目《{title}》，按要求生成对应内容。"}
+            ]    
+            
+            try:
+                raw_res = call_chat_completion(messages)
+                res = json.loads(raw_res)
+                if not save:
+                    return res
+                with open(save_path, 'w', encoding='utf-8') as f:
+                    json.dump(res, f, indent=2, ensure_ascii=False)
+            except json.JSONDecodeError:
+                print("不是有效的JSON格式")
+            
+        return wrapper
+    return decorator    # return is a decorated function
+
+
+@llm_json_generator(PAPER_STRUCTURE_PROMPT)
+def generate_paper_structure(title: str=None, save: bool=False, save_path: str=None) -> Dict:
+    print("Begin generating paper structure...")
+    pass
+
+@llm_json_generator(IMAGE_JSON_PROMPT)
+def generate_image_json(title: str=None, save: bool=False, save_path: str=None) -> Dict:
+    print("Begin generating figures' JSON...")
+    pass
