7eb79f63ccf886f5e56080086d475f66bef034a3
xin.lei <realleixin@qq.com>
2025-10-07 13:03:46 +0800

    refactor: introduce style_controller which is responsible for doc style

--- services/paper_service/src/service/doc/composer.py
+++ services/paper_service/src/service/doc/composer.py
@@ -1,9 +1,9 @@
 from docx import Document
 import os
-from src.service.doc.styles import _modify_heading_styles, _modify_normal_style, _modify_toc_styles
-from src.service.doc.styles import TOC_HEAD_STYLE, TOC_STYLE_1, TOC_STYLE_2, TOC_STYLE_3
-from src.service.doc.styles import HEADING_STYLE_2, HEADING_STYLE_3, HEADING_STYLE_4
-from src.service.doc.styles import NORMAL_STYLE
+from src.service.doc.styles import StyleController
+
+style_controller = StyleController()
+
 
 def init_doc(file_path: str=None):
     # initialize a doc object and styles
@@ -14,29 +14,29 @@ def init_doc(file_path: str=None):
         doc = Document()
         print(f"创建新文档: {file_path}")
 
-    _modify_toc_styles(doc)
-    _modify_normal_style(doc)
-    _modify_heading_styles(doc)
+    style_controller._modify_toc_styles(doc)
+    style_controller._modify_normal_style(doc)
+    style_controller._modify_heading_styles(doc)
 
     return doc
 
 def compose_toc(doc: Document, structure: dict):
     # generate table of contents in a docx
-    doc.add_paragraph("目录", style=TOC_HEAD_STYLE)
+    doc.add_paragraph("目录", style=style_controller.TOC_HEAD_STYLE)
 
     for chapter in structure["chapters"]:
         # 1-level
-        doc.add_paragraph(chapter["title"], style=TOC_STYLE_1)
+        doc.add_paragraph(chapter["title"], style=style_controller.TOC_STYLE_1)
 
         # 2-level
         if "sections" in chapter:
             for section in chapter["sections"]:
-                doc.add_paragraph(section["title"], style=TOC_STYLE_2)
+                doc.add_paragraph(section["title"], style=style_controller.TOC_STYLE_2)
 
                 # 3-level 
                 if "subsections" in section:
                     for subsection in section["subsections"]:
-                        doc.add_paragraph(subsection["title"], style=TOC_STYLE_3) 
+                        doc.add_paragraph(subsection["title"], style=style_controller.TOC_STYLE_3) 
     doc.add_page_break() 
 
 
@@ -44,34 +44,34 @@ def compose_main_body(doc: Document, structure: dict, main_body: dict):
     
     for chapter in structure["chapters"]:
         # 1-level Handling
-        doc.add_paragraph(chapter["title"], style=HEADING_STYLE_2)
+        doc.add_paragraph(chapter["title"], style=style_controller.HEADING_STYLE_2)
 
         if "sections" not in chapter:
             # Main Body     
             texts = main_body[chapter['title']]
             for text in texts:
-                doc.add_paragraph(text, style=NORMAL_STYLE) 
+                doc.add_paragraph(text, style=style_controller.NORMAL_STYLE) 
 
         # 2-level
         else:
             for section in chapter["sections"]:
                 # 2-level Handling
-                doc.add_paragraph(section['title'], style=HEADING_STYLE_3)
+                doc.add_paragraph(section['title'], style=style_controller.HEADING_STYLE_3)
 
                 if "subsections" not in section:
                     # Main Body
                     texts = main_body[section['title']]
                     for text in texts:
-                        doc.add_paragraph(text, style=NORMAL_STYLE)
+                        doc.add_paragraph(text, style=style_controller.NORMAL_STYLE)
 
                 # 3-level 
                 else:
                     for subsection in section["subsections"]:
                         # 3-level Handling
-                        doc.add_paragraph(subsection['title'], style=HEADING_STYLE_4)
+                        doc.add_paragraph(subsection['title'], style=style_controller.HEADING_STYLE_4)
                         # Main Body
                         texts = main_body[subsection['title']]
                         for text in texts:
-                            doc.add_paragraph(text, style=NORMAL_STYLE)
+                            doc.add_paragraph(text, style=style_controller.NORMAL_STYLE)
 
         doc.add_page_break() 
--- services/paper_service/src/service/doc/styles.py
+++ services/paper_service/src/service/doc/styles.py
@@ -4,132 +4,135 @@ from docx.enum.style import WD_STYLE_TYPE
 from docx.oxml.ns import qn
 from docx import Document
 
-TOC_HEAD_STYLE = "目录"
-TOC_STYLE_1 = "目录 1"
-TOC_STYLE_2 = "目录 2"
-TOC_STYLE_3 = "目录 3"
-NORMAL_STYLE = "正文"
-HEADING_STYLE_2 = "标题 2"
-HEADING_STYLE_3 = "标题 3"
-HEADING_STYLE_4 = "标题 4"
+class StyleController:
+
+    def __init__(self):
+
+        self.TOC_HEAD_STYLE = "目录"
+        self.TOC_STYLE_1 = "目录 1"
+        self.TOC_STYLE_2 = "目录 2"
+        self.TOC_STYLE_3 = "目录 3"
+        self.NORMAL_STYLE = "正文"
+        self.HEADING_STYLE_2 = "标题 2"
+        self.HEADING_STYLE_3 = "标题 3"
+        self.HEADING_STYLE_4 = "标题 4"
+
+    def _modify_toc_styles(self, doc: Document):
+        if self.TOC_HEAD_STYLE in doc.styles:
+            style = doc.styles[self.TOC_HEAD_STYLE]
+        else:
+            style = doc.styles.add_style(self.TOC_HEAD_STYLE, WD_STYLE_TYPE.PARAGRAPH)
+
+        style.font.name = "Times New Roman"
+        style.font.element.rPr.rFonts.set(qn('w:eastAsia'), '黑体')
+        style.font.size = Pt(16)
+        style.font.bold = True
+        style.paragraph_format.alignment = WD_PARAGRAPH_ALIGNMENT.CENTER
+
+        if self.TOC_STYLE_1 in doc.styles:
+            style = doc.styles[self.TOC_STYLE_1]
+        else:
+            style = doc.styles.add_style(self.TOC_STYLE_1, WD_STYLE_TYPE.PARAGRAPH)
+
+        style.font.size = Pt(12)  # 12磅（小四）
+        style.font.bold = True
+        style.font.name = "Times New Roman"
+        style.font._element.rPr.rFonts.set(qn('w:eastAsia'), '黑体')
+        style.paragraph_format.line_spacing = 1.25
+        style.paragraph_format.space_before = Pt(0)
+        style.paragraph_format.space_after = Pt(0)
+
+        if self.TOC_STYLE_2 in doc.styles:
+            style = doc.styles[self.TOC_STYLE_2]
+        else:
+            style = doc.styles.add_style(self.TOC_STYLE_2, WD_STYLE_TYPE.PARAGRAPH)
+
+        style.font.size = Pt(12)  # 12磅（小四）
+        style.font.bold = False
+        style.font.name = "Times New Roman"
+        style.font._element.rPr.rFonts.set(qn('w:eastAsia'), '宋体')
+        style.paragraph_format.line_spacing = 1.25
+        style.paragraph_format.space_before = Pt(0)
+        style.paragraph_format.space_after = Pt(0)
+        style.paragraph_format.left_indent = Pt(20)
+
+        if self.TOC_STYLE_3 in doc.styles:
+            style = doc.styles[self.TOC_STYLE_3]
+        else:
+            style = doc.styles.add_style(self.TOC_STYLE_3, WD_STYLE_TYPE.PARAGRAPH)
+
+        style.font.size = Pt(12)  # 12磅（小四）
+        style.font.bold = False
+        style.font.name = "Times New Roman"
+        style.font._element.rPr.rFonts.set(qn('w:eastAsia'), '宋体')
+        style.paragraph_format.line_spacing = 1.25
+        style.paragraph_format.space_before = Pt(0)
+        style.paragraph_format.space_after = Pt(0)
+        style.paragraph_format.left_indent = Pt(40)
 
 
-def _modify_toc_styles(doc: Document):
-    if TOC_HEAD_STYLE in doc.styles:
-        style = doc.styles[TOC_HEAD_STYLE]
-    else:
-        style = doc.styles.add_style(TOC_HEAD_STYLE, WD_STYLE_TYPE.PARAGRAPH)
+    def _modify_normal_style(self, doc: Document):
 
-    style.font.name = "Times New Roman"
-    style.font.element.rPr.rFonts.set(qn('w:eastAsia'), '黑体')
-    style.font.size = Pt(16)
-    style.font.bold = True
-    style.paragraph_format.alignment = WD_PARAGRAPH_ALIGNMENT.CENTER
+        if self.NORMAL_STYLE in doc.styles:
+            style = doc.styles[self.NORMAL_STYLE]
+        else:
+            style = doc.styles.add_style(self.NORMAL_STYLE, WD_STYLE_TYPE.PARAGRAPH)
+            
+        # font style
+        font_size = Pt(12)
+        style.font.size = font_size
+        style.font.bold = False
+        style.font.name = "Times New Roman"
+        style.element.rPr.rFonts.set(qn('w:eastAsia'), '宋体')
 
-    if TOC_STYLE_1 in doc.styles:
-        style = doc.styles[TOC_STYLE_1]
-    else:
-        style = doc.styles.add_style(TOC_STYLE_1, WD_STYLE_TYPE.PARAGRAPH)
+        # jusify alignment, first line indent 2 characters
+        style.paragraph_format.first_line_indent = font_size * 2 
+        style.paragraph_format.alignment = WD_PARAGRAPH_ALIGNMENT.JUSTIFY
 
-    style.font.size = Pt(12)  # 12磅（小四）
-    style.font.bold = True
-    style.font.name = "Times New Roman"
-    style.font._element.rPr.rFonts.set(qn('w:eastAsia'), '黑体')
-    style.paragraph_format.line_spacing = 1.25
-    style.paragraph_format.space_before = Pt(0)
-    style.paragraph_format.space_after = Pt(0)
+    def _modify_heading_styles(self, doc: Document):
 
-    if TOC_STYLE_2 in doc.styles:
-        style = doc.styles[TOC_STYLE_2]
-    else:
-        style = doc.styles.add_style(TOC_STYLE_2, WD_STYLE_TYPE.PARAGRAPH)
+        if self.HEADING_STYLE_2 in doc.styles:
+            style = doc.styles[self.HEADING_STYLE_2]
+        else:
+            style = doc.styles.add_style(self.HEADING_STYLE_2, WD_STYLE_TYPE.PARAGRAPH)
 
-    style.font.size = Pt(12)  # 12磅（小四）
-    style.font.bold = False
-    style.font.name = "Times New Roman"
-    style.font._element.rPr.rFonts.set(qn('w:eastAsia'), '宋体')
-    style.paragraph_format.line_spacing = 1.25
-    style.paragraph_format.space_before = Pt(0)
-    style.paragraph_format.space_after = Pt(0)
-    style.paragraph_format.left_indent = Pt(20)
+        # font
+        style.font.size = Pt(16)
+        style.font.bold = True
+        style.font.name = "Times New Roman"
+        style.element.rPr.rFonts.set(qn('w:eastAsia'), '黑体')
 
-    if TOC_STYLE_3 in doc.styles:
-        style = doc.styles[TOC_STYLE_3]
-    else:
-        style = doc.styles.add_style(TOC_STYLE_3, WD_STYLE_TYPE.PARAGRAPH)
+        # para
+        style.paragraph_format.alignment = WD_PARAGRAPH_ALIGNMENT.LEFT
+        style.paragraph_format.space_before = Pt(13)
 
-    style.font.size = Pt(12)  # 12磅（小四）
-    style.font.bold = False
-    style.font.name = "Times New Roman"
-    style.font._element.rPr.rFonts.set(qn('w:eastAsia'), '宋体')
-    style.paragraph_format.line_spacing = 1.25
-    style.paragraph_format.space_before = Pt(0)
-    style.paragraph_format.space_after = Pt(0)
-    style.paragraph_format.left_indent = Pt(40)
+        if self.HEADING_STYLE_3 in doc.styles:
+            style = doc.styles[self.HEADING_STYLE_3]
+        else:
+            style = doc.styles.add_style(self.HEADING_STYLE_3, WD_STYLE_TYPE.PARAGRAPH)
+
+        # font
+        style.font.size = Pt(14)
+        style.font.bold = True
+        style.font.name = "Times New Roman"
+        style.element.rPr.rFonts.set(qn('w:eastAsia'), '黑体')
+
+        # para
+        style.paragraph_format.alignment = WD_PARAGRAPH_ALIGNMENT.LEFT
+        style.paragraph_format.space_before = Pt(13)
 
 
-def _modify_normal_style(doc: Document):
+        if self.HEADING_STYLE_4 in doc.styles:
+            style = doc.styles[self.HEADING_STYLE_4]
+        else:
+            style = doc.styles.add_style(self.HEADING_STYLE_4, WD_STYLE_TYPE.PARAGRAPH)
 
-    if NORMAL_STYLE in doc.styles:
-        style = doc.styles[NORMAL_STYLE]
-    else:
-        style = doc.styles.add_style(NORMAL_STYLE, WD_STYLE_TYPE.PARAGRAPH)
-        
-    # font style
-    font_size = Pt(12)
-    style.font.size = font_size
-    style.font.bold = False
-    style.font.name = "Times New Roman"
-    style.element.rPr.rFonts.set(qn('w:eastAsia'), '宋体')
+        # font
+        style.font.size = Pt(12)
+        style.font.bold = True
+        style.font.name = "Times New Roman"
+        style.element.rPr.rFonts.set(qn('w:eastAsia'), '黑体')
 
-    # jusify alignment, first line indent 2 characters
-    style.paragraph_format.first_line_indent = font_size * 2 
-    style.paragraph_format.alignment = WD_PARAGRAPH_ALIGNMENT.JUSTIFY
-
-def _modify_heading_styles(doc: Document):
-
-    if HEADING_STYLE_2 in doc.styles:
-        style = doc.styles[HEADING_STYLE_2]
-    else:
-        style = doc.styles.add_style(HEADING_STYLE_2, WD_STYLE_TYPE.PARAGRAPH)
-
-    # font
-    style.font.size = Pt(16)
-    style.font.bold = True
-    style.font.name = "Times New Roman"
-    style.element.rPr.rFonts.set(qn('w:eastAsia'), '黑体')
-
-    # para
-    style.paragraph_format.alignment = WD_PARAGRAPH_ALIGNMENT.LEFT
-    style.paragraph_format.space_before = Pt(13)
-
-    if HEADING_STYLE_3 in doc.styles:
-        style = doc.styles[HEADING_STYLE_3]
-    else:
-        style = doc.styles.add_style(HEADING_STYLE_3, WD_STYLE_TYPE.PARAGRAPH)
-
-    # font
-    style.font.size = Pt(14)
-    style.font.bold = True
-    style.font.name = "Times New Roman"
-    style.element.rPr.rFonts.set(qn('w:eastAsia'), '黑体')
-
-    # para
-    style.paragraph_format.alignment = WD_PARAGRAPH_ALIGNMENT.LEFT
-    style.paragraph_format.space_before = Pt(13)
-
-
-    if HEADING_STYLE_4 in doc.styles:
-        style = doc.styles[HEADING_STYLE_4]
-    else:
-        style = doc.styles.add_style(HEADING_STYLE_4, WD_STYLE_TYPE.PARAGRAPH)
-
-    # font
-    style.font.size = Pt(12)
-    style.font.bold = True
-    style.font.name = "Times New Roman"
-    style.element.rPr.rFonts.set(qn('w:eastAsia'), '黑体')
-
-    # para
-    style.paragraph_format.alignment = WD_PARAGRAPH_ALIGNMENT.LEFT
-    style.paragraph_format.space_before = Pt(13)
+        # para
+        style.paragraph_format.alignment = WD_PARAGRAPH_ALIGNMENT.LEFT
+        style.paragraph_format.space_before = Pt(13)
