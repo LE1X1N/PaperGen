b483d2db6bf79dd3ed1613efb9546908ce1fd6b9
xin.lei <realleixin@qq.com>
2025-10-09 11:12:17 +0800

    feat: add argparse

--- services/paper_service/app.py
+++ services/paper_service/app.py
@@ -1,7 +1,14 @@
+import argparse
 from src.service.doc import Pipeline
 
+
 if __name__== "__main__":
-    title = "网络文学对传统文学的影响"
+
+    parser = argparse.ArgumentParser(description="generate thesis based on title")
+    parser.add_argument("--title", type=str, default="网络文学对传统文学的影响", help="title of your thesis")
+    args = parser.parse_args()
+
+    title = args.title
 
     pipeline = Pipeline()
-    pipeline.generate_paper(title, cache=True)
+    pipeline.generate_paper(title)
--- services/paper_service/src/service/doc/pipeline.py
+++ services/paper_service/src/service/doc/pipeline.py
@@ -35,7 +35,7 @@ class Pipeline:
         self.style_controller.init_doc_style(doc)
         return doc
 
-    def generate_paper(self, title: str, cache: bool=False):
+    def generate_paper(self, title: str):
         
         doc_folder = self.cache_dir / title
 
@@ -43,50 +43,61 @@ class Pipeline:
         if not doc_folder.exists():
             os.mkdir(doc_folder)
 
-        if not cache:
+        # structure
+        structure_path = doc_folder / self.STRUCTURE_JSON_FILE
+        if structure_path.exists():
+            structure = load_json(structure_path)
+            print(f"结构使用缓存文件：{structure_path}")
+        else:
             structure = self.json_generator.generate_paper_structure(query=f"请分析论文题目《{title}》，按要求生成对应JSON。", 
-                                                save=True, save_path= doc_folder / self.STRUCTURE_JSON_FILE)
-            print(f"结构生成成功！存储路径：{doc_folder / self.STRUCTURE_JSON_FILE}")
+                                                save=True, save_path=structure_path)
+            print(f"结构生成成功！存储路径：{structure_path}")
+
+        # abstract
+        abstract_path = doc_folder / self.ABSTRACT_JSON_FILE
+        if abstract_path.exists():
+            abstract = load_json(abstract_path)
+            print(f"引言使用缓存文件：{abstract_path}")
         else:
-            structure = load_json(doc_folder / "structure.json")
-            print(f"结构使用缓存文件：{doc_folder / self.STRUCTURE_JSON_FILE}")
-
-
-        if not cache:
             abstract = self.json_generator.generate_abstract_json(query=f"请分析论文题目《{title}》，按要求生成对应JSON。", 
-                                            save=True, save_path= doc_folder / self.ABSTRACT_JSON_FILE)
-            print(f"引言生成成功！存储路径：{doc_folder / self.ABSTRACT_JSON_FILE}")
-        else:
-            abstract = load_json(doc_folder / "abstract.json")
-            print(f"引言使用缓存文件：{doc_folder / self.ABSTRACT_JSON_FILE}")
+                                            save=True, save_path=abstract_path)
+            print(f"引言生成成功！存储路径：{abstract_path}")
 
-        if not cache:
+        # tables description
+        tables_desc_path = doc_folder / self.TABLES_DESC_JSON_FILE
+        if tables_desc_path.exists():
+            tables_desc = load_json(tables_desc_path)
+            print(f"表格描述使用缓存文件：{tables_desc_path}")
+        else:
             tables_desc = self.json_generator.generate_table_desc_json(query=f"请分析论文题目 《{title}》，按照提供的论文章节目录设计所需的表格映射表。本论文所有【论文章节】包括: 【{parse_section_titles(structure)}】",
-                                            save=True, save_path=doc_folder / self.TABLES_DESC_JSON_FILE)
-            print(f"表格描述生成成功！存储路径：{doc_folder / self.TABLES_DESC_JSON_FILE}")
-        else:
-            tables_desc = load_json(doc_folder / "tables_desc.json")
-            print(f"表格描述使用缓存文件：{doc_folder / self.TABLES_DESC_JSON_FILE}")
+                                            save=True, save_path=tables_desc_path)
+            print(f"表格描述生成成功！存储路径：{tables_desc_path}")
 
-        if not cache:
-            main_body = generate_main_body_text(title, structure, tables_desc, save=True, save_path = doc_folder / self.MAIN_BODY_JSON_FILE)
-            print(f"文章内容生成成功！存储路径：{doc_folder / self.MAIN_BODY_JSON_FILE}")
+        # main body
+        main_body_path = doc_folder / self.MAIN_BODY_JSON_FILE
+        if main_body_path.exists():
+            main_body = load_json(main_body_path)
+            print(f"文章内容使用缓存文件：{main_body_path}")
         else:
-            main_body = load_json(doc_folder / "main_body.json")
-            print(f"文章内容使用缓存文件：{doc_folder / self.MAIN_BODY_JSON_FILE}")
-        
-        if not cache:
-            tables = generate_tables(title, tables_desc, save = True, save_path = doc_folder / self.TABLES_JSON_FILE)
-            print(f"表格生成成功！{doc_folder / self.TABLES_JSON_FILE}")
-        else:
-            tables = load_json(doc_folder / "tables.json")
-            print(f"表格使用缓存文件：{doc_folder / self.TABLES_JSON_FILE}")
+            main_body = generate_main_body_text(title, structure, tables_desc, save=True, save_path=main_body_path)
+            print(f"文章内容生成成功！存储路径：{main_body_path}")
 
+        # tables
+        tables_path = doc_folder / self.TABLES_JSON_FILE
+        if tables_path.exists():
+            tables = load_json(tables_path)
+            print(f"表格使用缓存文件：{tables_path}")
+        else:
+            tables = generate_tables(title, tables_desc, save = True, save_path=tables_path)
+            print(f"表格生成成功！{tables_path}")
+
+
+        # create a docx file
         doc_path = doc_folder / f"{title}.docx"
         doc = self._init_doc(doc_path)
 
+        # compose content
         self.doc_composer.compose_all(doc, title, abstract, structure, main_body, tables)
         
         doc.save(doc_path)
-
         print(f"论文生成成功，文章保存路径：{doc_path}")
